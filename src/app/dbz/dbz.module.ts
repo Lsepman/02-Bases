74052106e302a17f5793178da04957c285ec4065
Lsepman <leticiasepulvedamancilla@gmail.com>
2024-11-27 16:27:57 +0100

    Creada Carpetas AddPersonaje y Lista dentro de DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main.-page.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { AddPersonajeComponent } from './components/add-personaje/add-personaje.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent,ListaComponent,AddPersonajeComponent],
   imports: [CommonModule],
   exports: [MainPageComponent]
 

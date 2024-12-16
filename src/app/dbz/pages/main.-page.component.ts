a10ce8334d3e97d4227495ccfdf6c6c6984caf19
Lsepman <leticiasepulvedamancilla@gmail.com>
2024-12-16 20:11:26 +0100

    Cambio en las acciones del main, cambio onDelete por delete y onNew por New

--- src/app/dbz/pages/main.-page.component.ts
+++ src/app/dbz/pages/main.-page.component.ts
@@ -28,13 +28,13 @@ export class MainPageComponent implements OnInit {
 ];
 
 //!Metodo para crear personaje con el metodo push
-public onNewPersonaje(personaje: Personaje): void{
+public NewPersonaje(personaje: Personaje): void{
   //console.log("MainPage")
   //console.log(personaje)
   this.personajes.push(personaje)
 }
 //!Metodo para eliminar personaje que se pasa por indice recibido. Usamos el splice donde indicamos el indice que queremos eliminar e indicamos que solo queremos eliminar 1
-public onDeletePersonaje(indice: number): void{
+public DeletePersonaje(indice: number): void{
    this.personajes.splice(indice,1)
 }
   constructor() { }
